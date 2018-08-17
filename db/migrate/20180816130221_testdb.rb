c7542826f0b064f54c2dd4e428b358d3f947bf59
Giorgi Nikolaishvili <giorgi.nikolaishvili25@gmail.com>
2018-08-17 11:38:26 +0400

    install bootstrap

---
+++ db/migrate/20180816130221_testdb.rb
@@ -0,0 +1,4 @@
+class Testdb < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_154219) do
+ActiveRecord::Schema.define(version: 2018_08_16_130221) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
