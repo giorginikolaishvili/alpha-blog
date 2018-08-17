3b1a5c85162285652ae9794033476d3f23a763d2
Giorgi Nikolaishvili <giorgi.nikolaishvili25@gmail.com>
2018-08-17 13:53:11 +0400

    added style to errors and flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article Updated"
+      flash[:success] = "Article Updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article Created"
+      flash[:success] = "Article Created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article destroyed"
+    flash[:danger] = "Article destroyed"
     redirect_to articles_path
   end
 
