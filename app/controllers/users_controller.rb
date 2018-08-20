2a270d071ba27f03555bea4279a2d3a8402ea85f
Giorgi Nikolaishvili <giorgi.nikolaishvili25@gmail.com>
2018-08-20 19:40:06 +0400

    add edit and update functionality :

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,18 @@ class UsersController <ApplicationController
             render 'new'
         end
     end
+    def edit
+        @user=User.find(params[:id])
+    end
+    def update
+        @user=User.find(params[:id])
+        if @user.update(set_params)
+            flash[:success]="User updated"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
     private
     def set_params
         params.require(:user).permit(:username,:email,:password)
