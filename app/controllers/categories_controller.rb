b9ff517f7255dcba65735826c482f723040753f7
Giorgi Nikolaishvili <giorgi.nikolaishvili25@gmail.com>
2018-08-23 13:32:10 +0400

    Add categories navbar and assotiation between category and article

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index,:show]
     def new
         @category=Category.new
     end
@@ -21,5 +22,12 @@ class CategoriesController < ApplicationController
     private
     def set_params
         params.require(:category).permit(:name)
+    end
+    def require_admin
+        if !logged_in?|| (logged_in? && !current_user.admin?)
+            flash[:danger]="only admin can do that"
+            redirect_to categories_path
+        end
+
     end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,17 +3,25 @@ require 'test_helper'
 class CategoriesControllerTest<ActionDispatch::IntegrationTest
     def setup
         @category=Category.create(name:"sports")
+        @user=User.create(username:"John",email:"john@gg.com",password:"password",admin:true)
     end
     test "should get categories index" do
         get categories_path
         assert_response :success
     end
     test "should get new" do
+        sign_in_as(@user,"password")
         get new_category_path
         assert_response :success
     end
     test "should get show" do
         get category_path(@category)
         assert_response :success
+    end
+    test "should redirect to create when admin not logged in" do
+    assert_no_difference "Category.count" do
+        post categories_path,params:{category:{name:"sports"}}
+    end
+    assert_redirected_to categories_path
     end
 end
